384c8b8c96a6704371cb7752a656cb818787e8f2
Hala Alsouly <u5995105@anu.edu.au>
2019-09-10 15:57:42 +1000

    implement get last location method

--- mobile/src/main/java/com/sap/digitalagedcare/Gps.java
+++ mobile/src/main/java/com/sap/digitalagedcare/Gps.java
@@ -1,5 +1,42 @@
 package com.sap.digitalagedcare;
 
 import android.content.Context;
+import android.location.Location;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class Gps {
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private static final String TAG = "GpsActivity";
+    private LocationRequest locationRequest;
+    private double lastLatitude;
+    private double lastLongitude;
+
+    public Gps(Context context){
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+    }
+
+    public void getLastLocation(){
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener( new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            lastLatitude=location.getLatitude();
+                            lastLongitude= location.getLongitude();
+                        }else {
+                            Log.e(TAG, "no location detected");
+                        }
+                    }
+                });
+    }
+
+
 }
