7faa4d64367e5f688c702ab0581bc3d0b48bb53b
Hala Alsouly <u5995105@anu.edu.au>
2019-09-18 13:04:38 +1000

    Ask for location permission on the main activity and also check if the device has gps

--- DigitalAgedCare/wear/src/main/java/com/sap/digitalagedcare/MainActivity.java
+++ DigitalAgedCare/wear/src/main/java/com/sap/digitalagedcare/MainActivity.java
@@ -1,12 +1,22 @@
 package com.sap.digitalagedcare;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.widget.TextView;
+
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
+    private static final String TAG = "MainActivity";
+    public static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 101;
+    GPSLocationTracking gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +24,41 @@ public class MainActivity extends WearableActivity {
         setContentView(R.layout.activity_main);
 
         mTextView = (TextView) findViewById(R.id.text);
+        if(hasGps()) {
+            gps= new GPSLocationTracking(this);
+            if (!hasPermission(this)) {
+                Log.e(TAG, "Permission denied!");
+                return;
+            }
+            gps.getLastLocation();
+            gps.createLocationRequest();
+        }else {
+            Log.e(TAG, "The device does not have gps");
+        }
 
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    /**
+     *Check if the device has gps feature
+     * @return True if gps available
+     */
+    boolean hasGps(){
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
+    }
+
+    /**
+     *Check the permission request result
+     * @param context
+     * @return True if user allow the location service
+     */
+    boolean hasPermission(Context context){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            }return false;
+        }
+        return true;
     }
 }
