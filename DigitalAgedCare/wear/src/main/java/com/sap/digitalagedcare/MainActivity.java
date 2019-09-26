74f25f4c3c0b051b8c1a25bf595ad4e672578aac
Yuzhao Li <u6724153@anu.edu.au>
2019-09-26 15:46:58 +1000

    finish fall detection

--- DigitalAgedCare/wear/src/main/java/com/sap/digitalagedcare/MainActivity.java
+++ DigitalAgedCare/wear/src/main/java/com/sap/digitalagedcare/MainActivity.java
@@ -1,21 +1,35 @@
 package com.sap.digitalagedcare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
 
-        // Enables Always-on
-        setAmbientEnabled();
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+
+    }
+
+    public void openActivity(){
+        Intent intent = new Intent(this, FallDetectionActivity.class);
+        startActivity(intent);
     }
 }
