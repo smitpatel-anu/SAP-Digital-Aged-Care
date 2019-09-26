f83775f5219a6f9c0d17272a5e9a01c97dcf0d24
Yuzhao Li <u6724153@anu.edu.au>
2019-09-26 14:31:13 +1000

    add fall detection activity for mobile

--- DigitalAgedCare/mobile/src/main/java/com/sap/digitalagedcare/MainActivity.java
+++ DigitalAgedCare/mobile/src/main/java/com/sap/digitalagedcare/MainActivity.java
@@ -2,13 +2,30 @@ package com.sap.digitalagedcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.b);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+
+    public void openActivity(){
+        Intent intent = new Intent(this, FallDetectionActivity.class);
+        startActivity(intent);
     }
 }
