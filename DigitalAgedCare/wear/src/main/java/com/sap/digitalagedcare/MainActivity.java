be8690d02a77c49928d6496ca00cc9e4722340c7
Hala Alsouly <u5995105@anu.edu.au>
2019-09-29 23:20:42 +1000

    create ServiceConnection to start and stop mylocationService

--- DigitalAgedCare/wear/src/main/java/com/sap/digitalagedcare/MainActivity.java
+++ DigitalAgedCare/wear/src/main/java/com/sap/digitalagedcare/MainActivity.java
@@ -1,10 +1,13 @@
 package com.sap.digitalagedcare;
 
 import android.Manifest;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
 import android.widget.TextView;
@@ -16,7 +19,7 @@ import com.sap.shared.GPSLocationTracking;
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
-    public MyLocationService gpsService;
+    public MyLocationService locationService;
     private static final String TAG = "MainActivity";
     public static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 101;
 
@@ -43,11 +46,31 @@ public class MainActivity extends WearableActivity {
      * @return True if user allow the location service
      */
     boolean hasPermission(Context context){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
             }return false;
         }
         return true;
     }
+
+    /**
+     *monitoring the state of MyLocationService.
+     */
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            String name = className.getClassName();
+            if (name.endsWith("MyLocationService")) {
+                locationService = ((MyLocationService.LocationServiceBinder) service).getService();
+            }
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            locationService = null;
+        }
+    };
 }
