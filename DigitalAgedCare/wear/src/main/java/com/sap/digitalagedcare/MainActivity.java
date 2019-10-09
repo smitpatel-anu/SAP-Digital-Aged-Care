eed59803745773899fb1905378560b115246f18e
Hala Alsouly <u5995105@anu.edu.au>
2019-10-09 19:58:52 +1100

    Get current location after fall event. Modified MainActivity (delete broadcastReceiver because we don't need to receive the location on the main activity.

--- DigitalAgedCare/wear/src/main/java/com/sap/digitalagedcare/MainActivity.java
+++ DigitalAgedCare/wear/src/main/java/com/sap/digitalagedcare/MainActivity.java
@@ -1,34 +1,23 @@
 package com.sap.digitalagedcare;
 
 import android.Manifest;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.core.app.ActivityCompat;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 
 public class MainActivity extends WearableActivity {
 
-
-    private TextView locationTv;
     public MyLocationService locationService;
     private static final String TAG = "MainActivity";
     public static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 101;
-    //public static final int MY_PERMISSIONS_REQUEST_COARSE_LOCATION = 102;
 
     private Button button;
 
@@ -37,17 +26,11 @@ public class MainActivity extends WearableActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (hasPermission(this)) {
-            final Intent intent = new Intent(this.getApplication(), MyLocationService.class);
-            this.getApplication().startService(intent);
-            this.getApplication().bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
-            LocalBroadcastManager.getInstance(MainActivity.this).registerReceiver(
-                    mMessageReceiver, new IntentFilter("GPSLocationUpdates"));
-        } else {
+        if (!hasPermission(this)) {
             Log.e(TAG, "Permission denied!");
         }
 
-        Button tremorTestActivityButton = (Button) findViewById(R.id.tremorTestActivityButton);
+        Button tremorTestActivityButton = findViewById(R.id.tremorTestActivityButton);
         tremorTestActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,7 +38,7 @@ public class MainActivity extends WearableActivity {
             }
         });
 
-        Button fallDetectionActivityButton = (Button) findViewById(R.id.fallDetectionActivityButton);
+        Button fallDetectionActivityButton = findViewById(R.id.fallDetectionActivityButton);
         fallDetectionActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,39 +70,6 @@ public class MainActivity extends WearableActivity {
         }
         return true;
     }
-
-    /**
-     * monitoring the state of MyLocationService.
-     */
-    private ServiceConnection serviceConnection = new ServiceConnection() {
-        public void onServiceConnected(ComponentName className, IBinder service) {
-            String name = className.getClassName();
-            if (name.endsWith("MyLocationService")) {
-                locationService = ((MyLocationService.LocationServiceBinder) service).getService();
-            }
-        }
-
-        public void onServiceDisconnected(ComponentName className) {
-            locationService = null;
-        }
-    };
-
-    /**
-     * receive an intent message from another class
-     */
-    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // Get extra data included in the Intent
-            Bundle b = intent.getBundleExtra("Location");
-            Location location = (Location) b.getParcelable("Location");
-            if (location != null) {
-
-                locationTv.setText("Latitude : " + location.getLatitude() + "\nLongitude : " + location.getLongitude());
-                Log.i(TAG, "The Location is*:" + location.getLatitude() + " " + location.getLongitude());
-            }
-        }
-    };
 
     public void onClickTremorTestActivityButton(View view) {
         startActivity(new Intent(MainActivity.this, TremorTestActivity.class));
